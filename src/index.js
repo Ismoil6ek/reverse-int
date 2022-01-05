8dd1699e9e6d30f9f2aa9b51d8ed4e610230f5bc
Ismoilbek Ilxomov <ismoil6ek@gmail.com>
2022-01-06 04:06:17 +0500

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    return (
+        // gets float number from integer
+        parseFloat(
+            n
+            // typecasting, from int to String
+            // 123 => "123"
+            .toString()
+            // creating an array from string 
+            // "123" => ["1", "2", "3"]
+            .split('')
+            // reverse of array
+            // ["1", "2", "3"] => ["3", "2", "1"]
+            .reverse()
+            // getting string from an array
+            // ["3", "2", "1"] => "321"
+            .join('')
+        ) * Math.sign(n) //to return the sign of number
+    )
+
 }
